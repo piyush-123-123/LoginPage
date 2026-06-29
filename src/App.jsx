b00f8d13c8d4125023f7e6db128f033efd804f0e
Your Name <your-email@example.com>
2026-06-29 17:19:39 +0530

    Implemented UseEffect first

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+// Write your code at relevant places in the code below:
+
+import React, { useEffect, useState } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+useEffect(()=>{
+  const storedUserLoggedInStatus=localStorage.getItem("isLoggedIn");
+  if(storedUserLoggedInStatus=="1"){
+    setIsLoggedIn(true);
+  }
+
+},[])
+  
+
+  const loginHandler = (email, password) => {
+    
+
+
+    setIsLoggedIn(true);
+    localStorage.setItem('isLoggedIn','1');
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
